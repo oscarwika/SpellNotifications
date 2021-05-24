0db0cfd651c186b7d45b111dc11dc6201562dab9
Oscar Wika <oscar_wika@hotmail.com>
2021-05-24 03:27:43 +0200

    Added sound if pet dies or something

--- SpellNotifications.lua
+++ SpellNotifications.lua
@@ -21,11 +21,6 @@ function SpellNotifications_OnEvent(event)
 	local timeStamp, event, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = CombatLogGetCurrentEventInfo()
 	--        1        2       3           4            5           6              7              8        9          10          11
 
-	if (checkEventAndSourceFlags(event, "SPELL_CAST_SUCCESS", sourceFlags, bit_band)) then
-		local spellName = select(13, CombatLogGetCurrentEventInfo());
-		addon.print("Event: "..spellName..".","purple","small")
-	end
-
 	-- SPELL_INTERRUPT
 	if (checkEventAndSourceFlags(event, "SPELL_INTERRUPT", sourceFlags, bit_band)) then
 		local extraSchool = select(17, CombatLogGetCurrentEventInfo())
@@ -35,32 +30,32 @@ function SpellNotifications_OnEvent(event)
 			spellSchool = "unknown spell school"
 		end
 
-		addon.print("Interrupted " .. string.lower(spellSchool) .. ".", "green", "small")
+		addon.showText("Interrupted " .. string.lower(spellSchool) .. ".", "green", "small")
 	end
 
 	-- SPELL_DISPEL
 	if (checkEventAndSourceFlags(event, "SPELL_DISPEL", sourceFlags, bit_band)) then
 		local spellName = select(16, CombatLogGetCurrentEventInfo());
 		if bit.band(destFlags, COMBATLOG_OBJECT_REACTION_FRIENDLY) > 0 then
-			addon.print("Dispelled "..spellName..".","white","small") -- friendly target
+			addon.showText("Dispelled "..spellName..".","white","small") -- friendly target
 		else
-			addon.print("Dispelled "..spellName..".","yellow","small") -- enemy target
+			addon.showText("Dispelled "..spellName..".","yellow","small") -- enemy target
 		end
 	end
 
 	-- SPELL_STOLEN
 	if (checkEventAndSourceFlags(event, "SPELL_STOLEN", sourceFlags, bit_band)) then
 		local spellName = select(16, CombatLogGetCurrentEventInfo());
-		addon.print("Stole "..spellName..".","yellow","small") -- enemy target
+		addon.showText("Stole "..spellName..".","yellow","small") -- enemy target
 	end
 
-	-- UNIT_DIED
-	if (checkEventAndSourceFlags(event, "UNIT_DIED", sourceFlags, bit_band)) then
+	-- UNIT_DIED or something
+	if ((event == "UNIT_DIED") or (event == "UNIT_DESTROYED") or (event == "UNIT_DESTROYED") and bit_band(sourceFlags, COMBATLOG_OBJECT_AFFILIATION_MINE) > 0 ) then
 		if bit_band(destFlags, COMBATLOG_OBJECT_TYPE_PET) > 0 then
-			addon.print("Pet dead.","red","large")
+			addon.showText("Pet dead.","red","large")
+			addon.playSound("buzz")
 		end
 	end
-
 end
 
 function checkEventAndSourceFlags(event, spellEvent, sourceFlags, bit_band)
